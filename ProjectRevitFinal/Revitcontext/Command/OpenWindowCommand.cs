427873f1e91567f4afb673d196c1a14411ffd2d2
Gamal Saied <gsaied12@gmail.com>
2024-01-23 21:27:33 +0200

    Add project files.

---
+++ ProjectRevitFinal/Revitcontext/Command/OpenWindowCommand.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using ProjectRevitFinal.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRevitFinal.Revitcontext.Command
+{
+    [Transaction(TransactionMode.Manual)]
+    public class OpenWindowCommand : IExternalCommand
+    {
+        #region Properties
+        public static Document doc { get; set; }
+        #endregion
+
+
+     public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+              doc = uidoc.Document;
+            try
+            {
+                MainUi mainui= new MainUi();
+                mainui.ShowDialog();
+
+
+
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+
+                message=ex.Message;
+                return Result.Failed;
+            }
+          
+        }
+    }
+}
---
+++ ProjectRevitFinal/View/MainUi.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ProjectRevitFinal.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectRevitFinal.View
+{
+    /// <summary>
+    /// Interaction logic for MainUi.xaml
+    /// </summary>
+    public partial class MainUi : Window
+    {
+        public MainUi()
+        {
+            InitializeComponent();
+            this.DataContext = new MainUiViewModel();
+        }
+    }
+}
