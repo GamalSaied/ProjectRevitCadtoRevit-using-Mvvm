a3bc4db8b2699e21e17468e4ec819b1062c93c4c
Muhammed Ahmed <sadaatvip@gmail.com>
2024-01-27 00:49:15 +0200

    Sadat_Columns_Usercontrol

--- ProjectRevitFinal/Revitcontext/Command/ImportCad.cs
+++ ProjectRevitFinal/Revitcontext/Command/ImportCad.cs
@@ -1,62 +1,68 @@
-﻿using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
-using Microsoft.Win32;
-using ProjectRevitFinal.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using ProjectRevitFinal.View;
 
 namespace ProjectRevitFinal.Revitcontext.Command
 {
-    public class ImportCad 
-    {
-       
-        public static void importfilcadpath(string filepath)
-        {
-            Document doc = OpenWindowCommand.doc;
 
+    public class ImportCad
+    {
+
+        public void GET_AutoCAD_FilePath()
+        {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "DWG Files|*.dwg|All Files|*.*";
             openFileDialog.Title = "Select DWG File";
 
+            //Step3: Insert File Path into Textbox 
             if (openFileDialog.ShowDialog() == true)
             {
+                //---------------------------------------------------------------------------------------------
                 //1# Get the selected file path and display it in the TextBox
-                string filePath = openFileDialog.FileName;
-
-                
-                 // Create DWG import options
-                 DWGImportOptions dwgOptions = new DWGImportOptions();
-                try
-                {
-
-                    var currentview = doc.ActiveView;
-                    ElementId elementid = null;
-                    using (Transaction tr = new Transaction(doc, "importfilcadpath"))
-                    {
-                        tr.Start();
-
-                        // Import the DWG file
-
-                        var loadpath = doc.Import(filePath, dwgOptions, currentview, out elementid);
-                        
-                       
-
-                        // Commit the transaction
-                        tr.Commit();
-
-                    }
-                }
-                catch (Exception ex)
-                {
-
-                    TaskDialog.Show("Error", "Failed to import DWG file. Error: " + ex.Message);
-                }
+                Columns.GetData.Path.Text = openFileDialog.FileName;
             }
         }
+        //public static void importfilcadpath(string filepath)
+        //{
+        //    Document doc = OpenWindowCommand.doc;
 
-      
+        //    OpenFileDialog openFileDialog = new OpenFileDialog();
+        //    openFileDialog.Filter = "DWG Files|*.dwg|All Files|*.*";
+        //    openFileDialog.Title = "Select DWG File";
+
+        //    if (openFileDialog.ShowDialog() == true)
+        //    {
+        //        //1# Get the selected file path and display it in the TextBox
+        //        string filePath = openFileDialog.FileName;
+
+
+        //         // Create DWG import options
+        //         DWGImportOptions dwgOptions = new DWGImportOptions();
+        //        try
+        //        {
+
+        //            var currentview = doc.ActiveView;
+        //            ElementId elementid = null;
+        //            using (Transaction tr = new Transaction(doc, "importfilcadpath"))
+        //            {
+        //                tr.Start();
+
+        //                // Import the DWG file
+
+        //                var loadpath = doc.Import(filePath, dwgOptions, currentview, out elementid);
+
+        //                // Commit the transaction
+        //                tr.Commit();
+
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+
+        //            TaskDialog.Show("Error", "Failed to import DWG file. Error: " + ex.Message);
+        //        }
+        //    }
+        //}
+
+
     }
 }
--- ProjectRevitFinal/View/Columns.xaml.cs
+++ ProjectRevitFinal/View/Columns.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace ProjectRevitFinal.View
 {
@@ -20,8 +7,13 @@ namespace ProjectRevitFinal.View
     /// </summary>
     public partial class Columns : UserControl
     {
+        private static Columns _Getdate;
+
+        public static Columns GetData { get => _Getdate; internal set => _Getdate = value; }
+
         public Columns()
         {
+            _Getdate = this;
             InitializeComponent();
         }
     }
