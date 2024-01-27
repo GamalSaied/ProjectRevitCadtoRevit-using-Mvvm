a066bf756cd259a199c7dd939a935247998598cb
Muhammed Ahmed <sadaatvip@gmail.com>
2024-01-27 18:22:48 +0200

    Sadat_Code FilterColumns NotComplete

---
+++ ProjectRevitFinal/Model/AutoCAD/AutoCAD_ColumnsType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProjectRevitFinal.Model.AutoCAD
+{
+    public class AutoCAD_ColumnsType
+    {
+        public int ID_Col;
+        public double Width;
+        public double Length;
+        public double Point_X;
+        public double Point_Y;
+        public String Section;
+
+    }
+
+}
---
+++ ProjectRevitFinal/Model/AutoCAD/GetColumns.cs
@@ -0,0 +1,119 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using ProjectRevitFinal.Model1;
+using ProjectRevitFinal.Revitcontext.Command;
+using ProjectRevitFinal.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectRevitFinal.Model.AutoCAD
+{
+    public class GetColumns
+    {
+
+        public static string SelectedLayer;
+        public void GetColumnsData()
+        {
+            //1-Attach Docment  + Selected Layer from Combobox
+            Document doc = OpenWindowCommand.doc;
+            SelectedLayer = Columns.GetData.AutoCAD_Layer_Columns.Text;
+            //2-Get Import Data from Drawing  
+            var cadelements = new FilteredElementCollector(doc).OfClass(typeof(ImportInstance)).WhereElementIsNotElementType().ToElementIds();
+            //3-Create List of Polylines 
+            List<PolyLine> polyLines = new List<PolyLine>();
+            //4-Filter Data [ Loop for every polyline ]
+            if (cadelements.Count > 0)
+            {
+                Options opt = new Options();
+                var Importainstance = doc.GetElement(cadelements.First()) as ImportInstance;
+                var geoelements = Importainstance.get_Geometry(opt);
+                foreach (GeometryObject item in geoelements)
+                {
+                    var geoinstance = item as GeometryInstance;
+                    var instancegeometry = geoinstance.GetInstanceGeometry();
+                    foreach (var instance in instancegeometry)
+                    {
+                        if (instance is PolyLine)
+                        {
+                            polyLines.Add(instance as PolyLine);
+                            foreach (var polyline in polyLines)
+                            {
+                                elementsLayers cadlayers = new elementsLayers();
+                                var polygraphicalid = doc.GetElement(polyline.GraphicsStyleId) as GraphicsStyle;
+                                cadlayers.Nameoflayer = polygraphicalid.GraphicsStyleCategory.Name;
+                                if (cadlayers.Nameoflayer == SelectedLayer)
+                                {
+                                    // Get Data from Polylines
+                                    Outline putline = polyline.GetOutline();
+                                    XYZ firstp = putline.MaximumPoint;
+                                    XYZ secondp = putline.MinimumPoint;
+                                    XYZ Linemid = midpoint(firstp.X, secondp.X, firstp.Y, secondp.Y, firstp.Z, secondp.Z);
+
+                                    Level collevel = null;
+                                    var elementsColumns = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Columns).WhereElementIsElementType().ToElements();
+                                    var levels = new FilteredElementCollector(doc).OfClass(typeof(Level)).Cast<Level>().ToList();
+                                    foreach (var level in levels)
+                                    {
+                                        if (level.Name == "Level 1")
+                                        {
+                                            collevel = level;
+                                        }
+                                    }
+                                    FamilySymbol fs = null;
+                                    foreach (var ele in elementsColumns)
+                                    {
+                                        if (ele.Name == SelectedColumntype.columntypes)
+                                        {
+                                            fs = ele as FamilySymbol;
+                                        }
+                                    }
+
+
+                                    using (Transaction trans = new Transaction(doc, "create columns"))
+                                    {
+
+                                        trans.Start();
+                                        try
+                                        {
+                                            if (!fs.IsActive)
+                                            {
+                                                fs.Activate();
+                                            }
+                                            doc.Create.NewFamilyInstance(Linemid, fs, collevel, StructuralType.NonStructural);
+
+                                        }
+                                        catch (Exception ex)
+                                        {
+
+                                            TaskDialog.Show(ex.Message, ex.ToString());
+                                        }
+
+                                        trans.Commit();
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+
+
+
+
+                }
+            }
+        }
+
+        //method to get the Midpoint of the column
+        private static XYZ midpoint(double x1, double x2, double y1, double y2, double z1, double z2)
+        {
+
+            XYZ midP = new XYZ((x1 + x2) / 2, (y1 + y2) / 2, (z1 + z2) / 2);
+            return midP;
+
+        }
+
+    }
+}
+
