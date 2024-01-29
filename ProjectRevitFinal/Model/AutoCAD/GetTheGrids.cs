b0b9c39b6f4b42bbf59d8e3066f63188719d6dab
Ahmed Hussein <ahmed9821hussein@gmail.com>
2024-01-29 19:17:12 +0200

    Add ITI icon

--- ProjectRevitFinal/Model/AutoCAD/GetTheGrids.cs
+++ ProjectRevitFinal/Model/AutoCAD/GetTheGrids.cs
@@ -1,142 +1,142 @@
-﻿using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Structure;
-using Autodesk.Revit.UI;
-using ProjectRevitFinal.Model1;
-using ProjectRevitFinal.Revitcontext.Command;
-using ProjectRevitFinal.View;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Controls;
-using System.Windows.Shapes;
-using System.Xml.Linq;
-using Line = Autodesk.Revit.DB.Line;
-using netDxf;
-using netDxf.Entities;
+﻿//using Autodesk.Revit.DB;
+//using Autodesk.Revit.DB.Structure;
+//using Autodesk.Revit.UI;
+//using ProjectRevitFinal.Model1;
+//using ProjectRevitFinal.Revitcontext.Command;
+//using ProjectRevitFinal.View;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Windows.Controls;
+//using System.Windows.Shapes;
+//using System.Xml.Linq;
+//using Line = Autodesk.Revit.DB.Line;
+//using netDxf;
+//using netDxf.Entities;
 
-namespace ProjectRevitFinal.Model.AutoCAD
-{
-    public class GetTheGrids
+//namespace ProjectRevitFinal.Model.AutoCAD
+//{
+//    public class GetTheGrids
 
-    {
-
-
-        public static List<elementsLayers> GetGridslayers()
-        {
-
-            Document doc = OpenWindowCommand.doc;
-            var cadelements = (IList<ElementId>)new FilteredElementCollector(doc).OfClass(typeof(ImportInstance)).WhereElementIsNotElementType().ToElementIds();
-
-            List<elementsLayers> Layeraxis = new List<elementsLayers>();
-
-            try
-            {
-                if (cadelements.Count > 0)
-                {
-                    Options opt = new Options();
-                    var Importainstance = doc.GetElement(cadelements.First()) as ImportInstance;
-                    var geoelements = Importainstance.get_Geometry(opt);
-
-                    foreach (GeometryObject item in geoelements)
-
-                    {
-                        if (item is GeometryInstance)
-                        {
-                            var geoinstance = item as GeometryInstance;
-                            var instancegeometry = geoinstance.GetInstanceGeometry();
+//    {
 
 
-                            if (geoelements != null)
-                            {
-                                foreach (var instance in instancegeometry)
-                                {
-                                    elementsLayers cadlayers = new elementsLayers();
-                                    if (instance is Line)
-                                    {
+//        public static List<elementsLayers> GetGridslayers()
+//        {
 
-                                        var polygrahicid = instance.GraphicsStyleId;
-                                        var axisline = doc.GetElement(polygrahicid) as GraphicsStyle;
-                                        cadlayers.Nameoflayer = axisline.GraphicsStyleCategory.Name;
+//            Document doc = OpenWindowCommand.doc;
+//            var cadelements = (IList<ElementId>)new FilteredElementCollector(doc).OfClass(typeof(ImportInstance)).WhereElementIsNotElementType().ToElementIds();
 
+//            List<elementsLayers> Layeraxis = new List<elementsLayers>();
 
-                                    }
-                                    Layeraxis.Add(cadlayers);
-                                }
+//            try
+//            {
+//                if (cadelements.Count > 0)
+//                {
+//                    Options opt = new Options();
+//                    var Importainstance = doc.GetElement(cadelements.First()) as ImportInstance;
+//                    var geoelements = Importainstance.get_Geometry(opt);
 
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
+//                    foreach (GeometryObject item in geoelements)
 
-                TaskDialog.Show("Error", " show the error" + ex.Message);
-
-            }
-            return Layeraxis;
-        }
-
-        public static void Createthegrids()
-        {
-            #region Loading Layers From dxf file
-            //Get Crids layer form the dxf file.
-            DxfDocument DxfFile = DxfDocument.Load(GlobalVariable.file);
-            IEnumerable<netDxf.Entities.Line> AxixCad = DxfFile.Lines.Where(ins => ins.Layer.Name == "Axix");
-            #endregion
-
-            #region Create Grids from CAD Axix
-            //Create the start and end grid point for each grid
-            XYZ GridStartPoint, GridEndPoint;
-
-            //Create a list to include all lines which represent the grids.
-            List<Curve> GridLines = new List<Curve>();
-
-            //Loop for all lines in the axix layer to draw a line and add to the GridLines list. 
-            for (int i = 0; i < AxixCad.Count(); i++)
-            {
-                //Get the lines from the Axix layer
-                netDxf.Entities.Line line = AxixCad.ElementAt(i);
-                //The start point of the axix layer line.
-                GridStartPoint = new XYZ(line.StartPoint.X, line.StartPoint.Y, line.StartPoint.Z);
-
-                //The end point of the axix layer line.
-                GridEndPoint = new XYZ(line.EndPoint.X, line.EndPoint.Y, line.EndPoint.Z);
-
-                //Create a line from the two point to represent the grid.
-                Autodesk.Revit.DB.Line GridLine = Autodesk.Revit.DB.Line.CreateBound(GridStartPoint, GridEndPoint);
-                GridLines.Add(GridLine); //Add the line to the GridLines list.
-            }
+//                    {
+//                        if (item is GeometryInstance)
+//                        {
+//                            var geoinstance = item as GeometryInstance;
+//                            var instancegeometry = geoinstance.GetInstanceGeometry();
 
 
+//                            if (geoelements != null)
+//                            {
+//                                foreach (var instance in instancegeometry)
+//                                {
+//                                    elementsLayers cadlayers = new elementsLayers();
+//                                    if (instance is Line)
+//                                    {
+
+//                                        var polygrahicid = instance.GraphicsStyleId;
+//                                        var axisline = doc.GetElement(polygrahicid) as GraphicsStyle;
+//                                        cadlayers.Nameoflayer = axisline.GraphicsStyleCategory.Name;
 
 
-            //Creating the Revit Grids.
+//                                    }
+//                                    Layeraxis.Add(cadlayers);
+//                                }
 
-            using (Transaction t = new Transaction(doc, "Draw Grids from dxf file"))
-            {
-                t.Start();
-                foreach (Autodesk.Revit.DB.Line line in GridLines)
-                {
-                    Grid.Create(doc, line);
-                }
-                t.Commit();
-            }
-            #endregion
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            catch (Exception ex)
+//            {
 
-            return Result.Succeeded;
-        }
-            catch (Exception e)
-            {
-                return Result.Failed;
-            }
-}
-    }
+//                TaskDialog.Show("Error", " show the error" + ex.Message);
 
-        }
+//            }
+//            return Layeraxis;
+//        }
 
-    }
-}
+//        public static void Createthegrids()
+//        {
+//            #region Loading Layers From dxf file
+//            //Get Crids layer form the dxf file.
+//            DxfDocument DxfFile = DxfDocument.Load(GlobalVariable.file);
+//            IEnumerable<netDxf.Entities.Line> AxixCad = DxfFile.Lines.Where(ins => ins.Layer.Name == "Axix");
+//            #endregion
+
+//            #region Create Grids from CAD Axix
+//            //Create the start and end grid point for each grid
+//            XYZ GridStartPoint, GridEndPoint;
+
+//            //Create a list to include all lines which represent the grids.
+//            List<Curve> GridLines = new List<Curve>();
+
+//            //Loop for all lines in the axix layer to draw a line and add to the GridLines list. 
+//            for (int i = 0; i < AxixCad.Count(); i++)
+//            {
+//                //Get the lines from the Axix layer
+//                netDxf.Entities.Line line = AxixCad.ElementAt(i);
+//                //The start point of the axix layer line.
+//                GridStartPoint = new XYZ(line.StartPoint.X, line.StartPoint.Y, line.StartPoint.Z);
+
+//                //The end point of the axix layer line.
+//                GridEndPoint = new XYZ(line.EndPoint.X, line.EndPoint.Y, line.EndPoint.Z);
+
+//                //Create a line from the two point to represent the grid.
+//                Autodesk.Revit.DB.Line GridLine = Autodesk.Revit.DB.Line.CreateBound(GridStartPoint, GridEndPoint);
+//                GridLines.Add(GridLine); //Add the line to the GridLines list.
+//            }
+
+
+
+
+//            //Creating the Revit Grids.
+
+//            using (Transaction t = new Transaction(doc, "Draw Grids from dxf file"))
+//            {
+//                t.Start();
+//                foreach (Autodesk.Revit.DB.Line line in GridLines)
+//                {
+//                    Grid.Create(doc, line);
+//                }
+//                t.Commit();
+//            }
+//            #endregion
+
+//            return Result.Succeeded;
+//        }
+//            catch (Exception e)
+//            {
+//                return Result.Failed;
+//            }
+//}
+//    }
+
+//        }
+
+//    }
+//}
 
 
 
--- ProjectRevitFinal/Revitcontext/Command/ExternalAPP.cs
+++ ProjectRevitFinal/Revitcontext/Command/ExternalAPP.cs
@@ -16,22 +16,20 @@ namespace ProjectRevitFinal.Revitcontext.Command
         {
             application.CreateRibbonTab("AEC Project");
 
+            // Create a new Ribbon Panel in Revit's UI
             RibbonPanel Panel = application.CreateRibbonPanel("AEC Project", "Draw elements");
 
+            // Get the current assembly for the command and resources
+            Assembly assembly = Assembly.GetExecutingAssembly();
 
-            string path = Assembly.GetExecutingAssembly().Location;
+            // Configure and create a button linked to a command class
+            PushButtonData p1 = new PushButtonData("BTN1", "CAD to Revit", assembly.Location, "ProjectRevitFinal.Revitcontext.Command.OpenWindowCommand");
+            PushButton pushp1 = Panel.AddItem(p1) as PushButton; // Add button to the Ribbon Panel
 
-            PushButtonData p1 = new PushButtonData("BTN1", "Drawcolumns", path, "ProjectRevitFinal.Revitcontext.Command.OpenWindowCommand");
-
-            // Load the image from resources
-            //var uriImage = new Uri("pack://application:,,,/ProjectRevitFinal.Revitcontext.Command;component/Styles1/Aec.png");
-            //var image = new BitmapImage(uriImage);
-
-            //// Set the image for the push button
-            //p1.LargeImage = image;
-
-            ////Add the push button to the Ribbon panel
-            Panel.AddItem(p1);
+            // Set up and assign an icon to the button
+            Uri UriPath = new Uri("pack://application:,,,/ProjectRevitFinal;component/Resources/iti-icon.png");
+            BitmapImage Image = new BitmapImage(UriPath);
+            pushp1.LargeImage = Image; // Assign icon to the button
 
             return Result.Succeeded;
         }
